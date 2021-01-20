e1413a9ab6387727d76e9abc2985ca9399f5b918
ekapob-sang <76510467+ekapob-sang@users.noreply.github.com>
2021-01-20 15:17:04 +0700

    Update test1.py

--- test1.py
+++ test1.py
@@ -11,14 +11,14 @@ from PIL import Image, ImageOps
 import numpy as np
 from keras.models import model_from_json
 
-# model_path ="E:\Practice deploy\catdogtiger_classifier\model.h5"
+# model_path ="Emodel.h5"
 # model = tf.keras.models.load_model(model_path)
 
-json_file = open('G:\My Drive\catdogtiger2\model_set2\model2.json', 'r')
+json_file = open('model2.json', 'r')
 model_json = json_file.read()
 json_file.close()
 model = model_from_json(model_json)
-model.load_weights('G:\My Drive\catdogtiger2\model_set2\model2_weights.h5')
+model.load_weights('model2_weights.h5')
 
 
 
