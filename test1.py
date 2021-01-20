4e7705056edb4db4667e7230fa51eccc2afd5215
ekapob-sang <76510467+ekapob-sang@users.noreply.github.com>
2021-01-20 11:42:49 +0700

    Update test1.py

--- test1.py
+++ test1.py
@@ -9,7 +9,7 @@ import tensorflow as tf
 import streamlit as st
 from PIL import Image, ImageOps
 import numpy as np
-from tensorflow.keras.models import load_mode
+from keras.models import load_mode
 
 
 model = load_model('model.h5')
