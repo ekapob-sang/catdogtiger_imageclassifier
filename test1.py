ad3e0d5dd7e910a1948b11b9e57204dde6678495
ekapob-sang <76510467+ekapob-sang@users.noreply.github.com>
2021-01-20 09:14:09 +0700

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 20 08:20:45 2021
+
+@author: admin-1309
+"""
+
+import tensorflow as tf
+import streamlit as st
+from PIL import Image, ImageOps
+import numpy as np
+
+
+model = tf.keras.models.load_model('model.h5')
+
+st.title("Dog / Cat / Tiger Classfication ")
